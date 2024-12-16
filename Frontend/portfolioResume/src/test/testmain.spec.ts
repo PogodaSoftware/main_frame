ed6f82b2c4875830cea498f8e02ce38e449cc43e
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-16 09:43:32 -0500

    Updated our folder structure to include correct pathing for our unit testing

--- Frontend/portfolioResume/src/test/testmain.spec.ts
+++ Frontend/portfolioResume/src/test/testmain.spec.ts
@@ -1,16 +1,12 @@
-describe('HelloWorldTest', () => {
-  let consoleSpy: jasmine.Spy;
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
 
-  beforeEach(() => {
-    // Spy on the console.log method
-    consoleSpy = spyOn(console, 'log');
-  });
-
-  it('should print Hello World to the console', () => {
-    // Call the console.log method
-    console.log('Hello World');
-
-    // Assert that console.log was called with 'Hello World'
-    expect(consoleSpy).toHaveBeenCalledWith('Hello World');
-  });
-});
+// Initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
