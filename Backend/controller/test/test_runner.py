b2440d0944a010225008863f1621e6aba7b9a8fc
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-18 13:17:16 -0500

    Changed the .yaml method to use pytest instead of unittest changed the test runner to ensure python is used

--- Backend/controller/test/test_runner.py
+++ Backend/controller/test/test_runner.py
@@ -1,14 +1,11 @@
+#!/usr/bin/env python3
 import unittest
 
 def run_all_tests():
-    """
-    Discover and run all test cases in the current directory.
-    """
-    # Discover tests in the current directory
+   
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover(start_dir='.', pattern='test_*.py')
 
-    # Run the tests
     test_runner = unittest.TextTestRunner(verbosity=2)
     test_runner.run(test_suite)
 
