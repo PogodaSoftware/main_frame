35608f9bdcfd9ccd0e94cd1ea4f9b4011933ef45
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-20 20:50:50 -0500

    Debugging

--- Playwright-tests/run_tests.py
+++ Playwright-tests/run_tests.py
@@ -11,9 +11,9 @@ def main():
         django.setup()
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            # "Couldn't import Django. Are you sure it's installed and "
+            # "available on your PYTHONPATH environment variable? Did you "
+            # "forget to activate a virtual environment?"
         ) from exc
 
     pytest_args = [
