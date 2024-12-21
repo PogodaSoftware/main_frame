b5d8d8b162c40968d7efc6ecce404eae77587107
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-20 21:02:41 -0500

    Adding the correct path for DJANGO_MODULES_SETTINGS

--- Backend/controller/manage.py
+++ Backend/controller/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_frame_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_frame.Backend.controller.main_frame_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
