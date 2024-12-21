813c0d814f0c57d78df9813a1a0a7ed0133807b7
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-20 21:07:12 -0500

    Debugging the path for settings.py

--- Playwright-tests/run_tests.py
+++ Playwright-tests/run_tests.py
@@ -4,7 +4,7 @@ import django
 import pytest
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_frame.main_frame.Backend.controller.main_frame_project.settings')  
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_frame_project.settings')  
 
     try:
         django.setup()
