a9674f72bc9dcc9f6016684ae0d18b5298e29d5e
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-22 18:03:21 -0500

    Debugging

--- Frontend/portfolioResume/src/test/testmain.spec.ts
+++ Frontend/portfolioResume/src/test/testmain.spec.ts
@@ -15,7 +15,7 @@ describe('Hello World Test', () => {
   it('should log "Hello World" to the console', () => {
     const consoleSpy = spyOn(console, 'log');
     console.log('Hello World');
-    expect(consoleSpy).toHaveBeenCalledWith('Hello Wold');
+    expect(consoleSpy).toHaveBeenCalledWith('Hello World');
   });
 });
 
