1cf625f83991a9bd18ff26b45f36c9c52311a45e
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-21 22:18:18 -0500

    Debugging

--- Frontend/portfolioResume/src/test/simpletest.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'zone.js/testing';
-
-describe('simple math test', () => {
-    it('should add 1 + 1 to the console', () => {
-        const consoleSpy = spyOn(console, 'log');
-        console.log(1 + 1);
-        expect(consoleSpy).toHaveBeenCalledWith(2);
-    });
-});
--- Frontend/portfolioResume/src/test/testmain.spec.ts
+++ Frontend/portfolioResume/src/test/testmain.spec.ts
@@ -1,10 +1,27 @@
 import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
 
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
 
 describe('Hello World Test', () => {
   it('should log "Hello World" to the console', () => {
     const consoleSpy = spyOn(console, 'log');
     console.log('Hello World');
     expect(consoleSpy).toHaveBeenCalledWith('Hello World');
+  });
+});
+
+describe('simple math test', () => {
+  it('should add 1 + 1 to the console', () => {
+      const consoleSpy = spyOn(console, 'log');
+      console.log(1 + 1);
+      expect(consoleSpy).toHaveBeenCalledWith(2);
   });
 });
