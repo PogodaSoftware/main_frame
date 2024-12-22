96cbc11803616c712fb884a984ca75a5c32e20a9
Jaroslaw Pogoda <jarek1330@gmail.com>
2024-12-21 20:48:25 -0500

    Fix JSON formatting for Jira comment using jq

--- jira-update.js
+++ jira-update.js
@@ -11,7 +11,7 @@ const commentBody = `Pull request merged: [${PULL_REQUEST_URL}]\nLatest commit: 
 const data = JSON.stringify({ body: commentBody });
 
 const options = {
-  hostname: JIRA_BASE_URL.replace('https://', '').replace('http://', ''),
+  hostname: JIRA_BASE_URL,
   path: `/rest/api/3/issue/${ISSUE_KEY}/comment`,
   method: 'POST',
   headers: {
