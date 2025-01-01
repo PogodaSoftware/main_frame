eafe728d2deb4c82fac4a2ed694a5e6194e6ace0
kevin ortiz <kevin.ortiz@fdmgroup.com>
2025-01-01 15:49:38 -0500

    Testing angular unit test to ensure correct url is being reached

--- Frontend/portfolioResume/src/app/app.component.spec.ts
+++ Frontend/portfolioResume/src/app/app.component.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [HttpClientTestingModule], // Import the testing module
+      declarations: [AppComponent],      // Declare the AppComponent
     }).compileComponents();
+
+    // Inject the testing controller and HttpClient
+    httpTestingController = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+  });
+
+  afterEach(() => {
+    // Verify that no unmatched HTTP requests are pending
+    httpTestingController.verify();
   });
 
   it('should create the app', () => {
@@ -14,5 +29,29 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it('should reach http://localhost:80/kevin', (done) => {
+    const testUrl = 'http://localhost:80/kevin';
 
+    httpClient.get(testUrl).subscribe((response) => {
+      expect(response).toBeTruthy();
+      done();
+    });
+
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush({ success: true }); 
+  });
+
+  it('should reach http://localhost:80/fake', (done) => {
+    const testUrl = 'http://localhost:80/fake';
+
+    httpClient.get(testUrl).subscribe((response) => {
+      expect(response).toBeTruthy();
+      done();
+    });
+
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush({ success: true }); 
+  });
 });
