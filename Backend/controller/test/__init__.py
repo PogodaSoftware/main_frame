84525d10870d3e5728f6839b5b6a0cf67d646bc3
kevin ortiz <kevin.ortiz@fdmgroup.com>
2024-12-31 21:35:42 -0500

    Fixing pathing issues

--- Backend/controller/test/__init__.py
+++ Backend/controller/test/__init__.py
@@ -1,9 +1,10 @@
 import pytest
 
+
 from Backend.controller import main_frame_project
 from Backend.controller.test import hello
 
 [pytest]
 
-DJANGO_SETTINGS_MODULE = main_frame.Backend.controller.main_frame_project.settings
+DJANGO_SETTINGS_MODULE = main_frame_project.Backend.controller.main_frame_project.settings
 python_files = hello.py
