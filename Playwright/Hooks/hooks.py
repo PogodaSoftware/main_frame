4dc6d3f5ab1d923dccea422c41292e0f0ab394a5
kevin ortiz <kevin.ortiz@fdmgroup.com>
2025-01-04 18:03:24 -0500

    Testing

--- Playwright/Hooks/hooks.py
+++ Playwright/Hooks/hooks.py
@@ -4,7 +4,6 @@ def selecting_different_routes(page, route):
     if route == 'kevin':
         page.goto(f"http://localhost:{frontend_port}/kevin")
     elif route == 'pogoda':
-        
         page.goto(f"http://localhost:{frontend_port}/pogoda")
     else:
         raise ValueError(f"Unknown route: {route}")
