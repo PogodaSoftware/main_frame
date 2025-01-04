6a876439eef414550120cdc5ffaf4c769211aced
kevin ortiz <kevin.ortiz@fdmgroup.com>
2025-01-04 17:06:39 -0500

    Testing

--- Playwright/steps/test_pogoda_home_page.py
+++ Playwright/steps/test_pogoda_home_page.py
@@ -17,9 +17,7 @@ def page():
 @given("I navigate to pogoda home page")
 def navigate_to_pogoda_home_page(page):
     frontend_port = os.getenv('FRONTEND_PORT')
-    if not frontend_port:
-        raise ValueError("FRONTEND_PORT environment variable is not set")
-    page.goto(f"http://localhost:{frontend_port}/pogoda")
+page.goto(f"http://localhost:{frontend_port}/kevin")
 
 @then(parsers.parse('it should display the text "{text}"'))
 def verify_text(page, text):
