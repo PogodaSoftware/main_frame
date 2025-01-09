118d84953f2247d45108d8aac3d262158c837fe0
kevin ortiz <kevin.ortiz@fdmgroup.com>
2025-01-09 14:31:33 -0500

    Updating the karma.conf.js port

--- Frontend/portfolioResume/src/test/karma.conf.js
+++ Frontend/portfolioResume/src/test/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
         ]
       },
       reporters: ['progress', 'kjhtml'],
-      port: "${FRONTEND_PORT}",
+      port: 80,
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
