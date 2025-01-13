8f14f7d1e2fbeb0ce72de55275fb259cd4449ef7
Kevin Ortiz <kevin.ortiz.software@gmail.com>
2025-01-12 20:03:56 -0500

    Testing playwright tests with GitHub secrets

--- Playwright/Hooks/hooks.py
+++ Playwright/Hooks/hooks.py
@@ -1,20 +1,21 @@
 import os
 from playwright.sync_api import sync_playwright
-frontend_port = os.getenv('FRONTEND_PORT')
-
 
 def page():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
+        browser = playwright.chromium.launch(headless=False, slow_mo=500)  # Slow down actions by 500 milliseconds
         page = browser.new_page()
         yield page
         browser.close()
+
 def selecting_different_routes(page, route):
+    frontend_port = os.getenv('FRONTEND_PORT')
+    if not frontend_port:
+        raise ValueError("FRONTEND_PORT environment variable is not set")
+    
     if route == 'kevin':
         page.goto(f"http://localhost:{frontend_port}/kevin")
     elif route == 'pogoda':
         page.goto(f"http://localhost:{frontend_port}/pogoda")
     else:
         raise ValueError(f"Unknown route: {route}")
- 
- 
