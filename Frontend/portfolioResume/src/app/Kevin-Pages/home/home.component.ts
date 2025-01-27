d3e76772d36d48b9723567cf0c76584bca402545
Kevin Ortiz <kevin.ortiz.software@gmail.com>
2025-01-27 12:27:02 -0500

    Added a professional picture to the homepage.

--- Frontend/portfolioResume/src/app/Kevin-Pages/home/home.component.ts
+++ Frontend/portfolioResume/src/app/Kevin-Pages/home/home.component.ts
@@ -4,40 +4,46 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   template: `
-  <head>
-    <title>Kevin's portfolio project</title>
-  </head>
-  <body>
-    <nav id="desktop-navigation">
-      <div class="logo">Kevin Ortiz!</div>
-      <div>
-        <ul class="nav-links">
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contacts">Contacts</a></li>
-        </ul>
-      </div>
-    </nav>
+    <head>
+      <title>Kevin's portfolio project</title>
+    </head>
+    <body>
+      <nav id="desktop-navigation">
+        <div class="logo">Kevin Ortiz!</div>
+        <div>
+          <ul class="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contacts">Contacts</a></li>
+          </ul>
+        </div>
+      </nav>
 
-    <nav id="hamburger-navigation">
-      <div class="logo">kevin</div>
-      <div class="hamburger-menu">
-        <div class="hamburger-icon" (click)="toggleMenu()">
-          <span></span>
-          <span></span>
-          <span></span>
+      <nav id="hamburger-navigation">
+        <div class="logo">Kevin Ortiz!</div>
+        <div class="hamburger-menu">
+          <div class="hamburger-icon" (click)="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="menu-links">
+            <li><a href="#about" (click)="toggleMenu()">About</a></li>
+            <li><a href="#experience" (click)="toggleMenu()">Experience</a></li>
+            <li><a href="#projects" (click)="toggleMenu()">Projects</a></li>
+            <li><a href="#contacts" (click)="toggleMenu()">Contacts</a></li>
+          </div>
         </div>
-        <div class="menu-links">
-          <li><a href="#about" (click)="toggleMenu()">About</a></li>
-          <li><a href="#experience" (click)="toggleMenu()">Experience</a></li>
-          <li><a href="#projects" (click)="toggleMenu()">Projects</a></li>
-          <li><a href="#contacts" (click)="toggleMenu()">Contacts</a></li>
+      </nav>
+
+      <section id="profile">
+        <div class="section__picture-container">
+          <img src="./assets/professional-picture.jpg" alt="kevin ortiz profile picture" />
         </div>
-      </div>
-    </nav>
-  </body>
-`,
+      </section>
+    </body>
+  `,
   styleUrls: ['./home.component.scss', './media-queries.component.scss'],
 })
 export class KevinHomeComponent {
