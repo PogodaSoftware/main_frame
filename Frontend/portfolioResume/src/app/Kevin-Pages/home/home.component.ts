c31e8b468db04ebf1c909f2509bae0e4bb31cb50
Kevin Ortiz <kevin.ortiz.software@gmail.com>
2025-01-27 16:00:55 -0500

    I have added an image to the homepage

--- Frontend/portfolioResume/src/app/Kevin-Pages/home/home.component.ts
+++ Frontend/portfolioResume/src/app/Kevin-Pages/home/home.component.ts
@@ -38,8 +38,45 @@ import { Component } from '@angular/core';
       </nav>
 
       <section id="profile">
-        <div class="section__picture-container">
-          <img src="./assets/professional-picture.jpg" alt="kevin ortiz profile picture" />
+        <div class="section-picture-container">
+          <img
+            src="./assets/professional-picture.jpg"
+            alt="kevin ortiz profile picture"
+          />
+        </div>
+
+        <div class="section-text">
+          <p class="section-text-p1">Hello I'm</p>
+          <h1 class="title">Kevin Ortiz</h1>
+          <p class="section-text-p2">Quality Assurance and DevOps Engineer</p>
+          <div class="btn-container">
+            <button class="btn btn-color-2" (click)="openResume()">
+              Download CV
+            </button>
+
+            <button
+              class="btn btn-color-1"
+              onclick="location.href='./#contacts'"
+            >
+              Contact Me
+            </button>
+          </div>
+
+          <div id="socials-container">
+            <img
+              src="./assets/linkedin.png"
+              alt="My LinkedIn profile"
+              class="icon"
+              onclick="location.href='https://www.linkedin.com/in/kevino73/'"
+            />
+
+            <img
+              src="./assets/github.png"
+              alt="My GitHub profile"
+              class="icon"
+              onclick="location.href='https://github.com/kevinortiz43'"
+            />
+          </div>
         </div>
       </section>
     </body>
@@ -47,6 +84,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss', './media-queries.component.scss'],
 })
 export class KevinHomeComponent {
+  openResume(): void {
+    window.open('./assets/Ortiz_Kevin_Resume.pdf');
+  }
+
   toggleMenu(): void {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
