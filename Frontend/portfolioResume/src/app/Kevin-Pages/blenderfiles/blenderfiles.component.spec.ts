955d492f5b6254e70426af613659031d88f4126d
Kevin <kevin.ortiz.software@gmail.com>
2025-04-12 21:02:49 -0400

    Updated the blendefiles.component.spec.ts to introduce activatedroute and convertToParamMap dependencies

--- Frontend/portfolioResume/src/app/Kevin-Pages/blenderfiles/blenderfiles.component.spec.ts
+++ Frontend/portfolioResume/src/app/Kevin-Pages/blenderfiles/blenderfiles.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { KevinBlenderFilesComponent } from './blenderfiles.component';
 
 describe('KevinBlenderFilesComponent', () => {
@@ -8,9 +8,18 @@ describe('KevinBlenderFilesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [KevinBlenderFilesComponent]
-    })
-    .compileComponents();
+      declarations: [KevinBlenderFilesComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({})
+            }
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(KevinBlenderFilesComponent);
     component = fixture.componentInstance;
