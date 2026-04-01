fd892c5f8fd2fd28bfb0c24d0ba20fb72803932c
Kevin Ortiz <93737536+kevinortiz43@users.noreply.github.com>
2026-03-31 20:42:34 -0400

    feat(bff): add BFF SDUI layer for Beauty app

--- Backend/controller/main_frame_project/urls.py
+++ Backend/controller/main_frame_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/pogoda/', include('pogoda_api.urls')),
     path('api/beauty/', include('beauty_api.urls')),
+    path('api/bff/', include('bff_api.urls')),
 ]
